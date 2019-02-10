2d3796c5f3a32195568e8798e221fb972c02989b
R-yz <1239855084@qq.com>
2019-02-10 18:04:22 +0800

    reports.vue

---
+++ reports/reports.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="echart">
+
+    </div>
+</template>
+
+<script>
+export default {
+    mounted() {
+        // 基于准备好的dom，初始化echarts实例
+        var myChart = this.$echarts.init(document.getElementById('echart'));
+
+        // 指定图表的配置项和数据
+        var option = {
+            title: {
+                text: '图表'
+            },
+            tooltip: {},
+            legend: {
+                data: ['销量']
+            },
+            xAxis: {
+                data: ["a", "b", "c", "d", "e", "f"]
+            },
+            yAxis: {},
+            series: [{
+                name: '销量',
+                type: 'bar',
+                data: [6, 23, 2, 43, 76, 23]
+            }]
+        };
+
+        // 使用刚指定的配置项和数据显示图表。
+        myChart.setOption(option);
+    }
+}
+</script>
+
+<style>
+#echart {
+    width: 100%;
+    height: 100%;
+}
+</style>
